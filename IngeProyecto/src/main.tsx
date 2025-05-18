b73a081784c5b8231b576f6d9a5dd6b740ecb546
AgustinPatun <agusplatun@hotmail.com>
2025-05-18 20:25:39 -0300

    Se separaron las rutas del main para que quede un codigo mas limpio

--- IngeProyecto/src/main.tsx
+++ IngeProyecto/src/main.tsx
@@ -1,53 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
-import Servicios from './pages/Servicios.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
-import MiCuenta from './pages/myAccount.tsx';
-import AltaMaquinaria from './pages/altaMaquinaria.tsx';
-import Contacto from './pages/contacto.tsx';
-import BajaMaquinaria from './pages/bajaMaquinaria.tsx';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App></App>
-  },
-  {
-    path: "/servicios",
-    element: <Servicios></Servicios>
-  },
-  {
-    path:"/login",
-    element:<Login></Login>
-  },
-  {
-    path:"/register",
-    element:<Register></Register>
-  },
-  {
-    path:"/mis-datos",
-    element:<MiCuenta></MiCuenta>
-  },
-  {
-    path:"/alta-maquinaria",
-    element:<AltaMaquinaria></AltaMaquinaria>
-  },
-  {
-    path:"/contacto",
-    element:<Contacto></Contacto>
-  },
-  {
-    path:"/baja-maquinaria",
-    element:<BajaMaquinaria></BajaMaquinaria>
-  }
-
-])
 
 ReactDOM.render(
   <React.StrictMode>
